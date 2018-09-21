7cac1168f743f0ff7e38326a7cb76768011f94c2
Daniel Shell <dshell321@gmail.com>
2018-09-21 16:37:38 +1000

    Re-created skeleton of List page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBellSlash } from '@fortawesome/free-solid-svg-icons';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'codestroke-angular';
+    icon_bars = faBars;
+    icon_bell_slash = faBellSlash;
+    icon_signout = faSignOutAlt;
 }
