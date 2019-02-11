dbf7d5ba7a90ab6003cfad6b30c8577205b2efbb
Daniel Shell <dshell321@gmail.com>
2019-02-11 20:42:29 +1100

    Case lists are sorted by the times provided (eta, active_time, etc.)

--- src/app/cases/list-page/list-container/list-container.component.ts
+++ src/app/cases/list-page/list-container/list-container.component.ts
@@ -15,6 +15,41 @@ export class ListContainerComponent implements OnInit {
     @Input()
     set cases( list : CaseDetails[]) {
         this._cases = list.filter(c => c.status == this.status);
+        let sortFn;
+        switch (this.status) {
+            case "incoming":
+                // Orders by the earliest ETA
+                // If an ETA is null, it should go to the bottom
+                sortFn = (a,b) => {
+                    if (!a.eta && !b.eta) {
+                        return 0;
+                    }
+                    if (!a.eta) {
+                        return 1;
+                    }
+                    if (!b.eta) {
+                        return -1;
+                    }
+
+                    return new Date(a.eta).getTime() - new Date(b.eta).getTime();
+                }
+                break;
+            case "active":
+                // Orders by the latest arrival time (ie. most recently arrived)
+                // If a time is null, it should go to the bottom
+                sortFn = (a,b) => {
+                    return new Date(b.active_timestamp).getTime() - new Date(a.active_timestamp).getTime();
+                }
+                break;
+            case "completed":
+                // Orders by the latest completion time (ie. most recently completed)
+                // If a time is null, it should go to the bottom
+                sortFn = (a,b) => {
+                    return new Date(b.completed_timestamp).getTime() - new Date(a.completed_timestamp).getTime();
+                }
+                break;
+        }
+        this._cases.sort(sortFn);
     }
     get cases() {
         return this._cases;
