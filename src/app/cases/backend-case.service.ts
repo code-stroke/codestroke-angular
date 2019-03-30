2050ef976704a81dce2d3c8c4a62263c54347f1b
Daniel Shell <dshell321@gmail.com>
2019-03-30 17:42:51 +1100

    Implement viewing events by date

--- src/app/cases/backend-case.service.ts
+++ src/app/cases/backend-case.service.ts
@@ -13,39 +13,39 @@ import { LoadingService } from '../loading.service';
     providedIn: 'root'
 })
 export class BackendCaseService {
-    cases : Array<CaseDetails>
+    cases: Array<CaseDetails>;
 
-    constructor(private api : ApiService, private loading : LoadingService) { }
+    constructor(private api: ApiService, private loading: LoadingService) { }
 
-    getCases() : Observable<CaseDetails[]> {
-        //TODO: Error Handling
+    getCases(): Observable<CaseDetails[]> {
+        // TODO: Error Handling
         this.loading.showLoading();
-        return this.api.performRequest("get", "/cases/view/").pipe(
+        return this.api.performRequest('get', '/cases/view/').pipe(
             // Hide the loading when results come back (either normal or error)
             tap(() => this.loading.hideLoading(), () => this.loading.hideLoading()),
-            map((data) => data["result"].map(item => CaseDetails.create(item)))
+            map((data) => data['result'].map(item => CaseDetails.create(item)))
         );
     }
 
-    getCase(id : number, table : string) {
+    getCase(id: number, table: string) {
         this.loading.showLoading();
-        return this.api.performRequest("get", `/${table}/${id}/view/`).pipe(
+        return this.api.performRequest('get', `/${table}/${id}/view/`).pipe(
             tap(() => this.loading.hideLoading(), () => this.loading.hideLoading()),
-            map((data) => { if (data) return data["result"][0]})
+            map((data) => { if (data) { return data['result'][0]; }})
         );
     }
 
-    updateCase(id : number, table : string, data : any) {
+    updateCase(id: number, table: string, data: any) {
         this.loading.showLoading();
-        return this.api.performRequest("put", `/${table}/${id}/edit/`, data).pipe(
+        return this.api.performRequest('put', `/${table}/${id}/edit/`, data).pipe(
             tap(() => this.loading.hideLoading(), () => this.loading.hideLoading()),
             tap((data) => console.log(data))
         );
     }
 
-    addCase(data : CaseDetails) {
+    addCase(data: CaseDetails) {
         this.loading.showLoading();
-        return this.api.performRequest("post", `/cases/add/`, data).pipe(
+        return this.api.performRequest('post', `/cases/add/`, data).pipe(
             tap(() => this.loading.hideLoading(), () => this.loading.hideLoading()),
             tap((data) => console.log(data))
         );
@@ -54,29 +54,41 @@ export class BackendCaseService {
     getEvents(data: any): Observable<any> {
         switch (data.type) {
             case 'date':
-                //TODO
+                let date_params = new HttpParams();
+                date_params = date_params.set('start', data.start);
+                date_params = date_params.set('end', data.end);
+
+                console.log(date_params.toString());
+
+                return this.api.performRequest('get', '/event_log/datetime/', null, date_params).pipe(
+                    // Hide the loading when results come back (either normal or error)
+                    tap(() => this.loading.hideLoading(), () => this.loading.hideLoading()),
+                    tap((data) => console.log(data)),
+                    map((data) => data['result']),
+                    
+                );
                 break;
             case 'limit':
             default:
-                let params = new HttpParams();
+                let lim_params = new HttpParams();
                 if (data.start) {
-                    params = params.set("start", data.start);
+                    lim_params = lim_params.set('start', data.start);
                 } else {
-                    params = params.set("start", "1");
+                    lim_params = lim_params.set('start', '1');
                 }
 
                 if (data.number) {
-                    params = params.set("number", data.number);
+                    lim_params = lim_params.set('number', data.number);
                 } else {
-                    params = params.set("number", "50");
+                    lim_params = lim_params.set('number', '50');
                 }
 
-                console.log(params.toString());
+                console.log(lim_params.toString());
 
-                return this.api.performRequest("get", "/event_log/limit/", null, params).pipe(
+                return this.api.performRequest('get', '/event_log/limit/', null, lim_params).pipe(
                     // Hide the loading when results come back (either normal or error)
                     tap(() => this.loading.hideLoading(), () => this.loading.hideLoading()),
-                    map((data) => data["result"]),
+                    map((data) => data['result']),
                     tap((data) => console.log(data))
                 );
         }
--- src/app/cases/event-page/event-date/event-date.component.ts
+++ src/app/cases/event-page/event-date/event-date.component.ts
@@ -1,15 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, LOCALE_ID } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { BackendCaseService } from '../../backend-case.service';
+import { LoadingService } from 'src/app/loading.service';
+import { BehaviorSubject } from 'rxjs';
+import { formatDate } from '@angular/common';
 
 @Component({
-  selector: 'cs-event-date',
-  templateUrl: './event-date.component.html',
-  styleUrls: ['./event-date.component.scss']
+    selector: 'cs-event-date',
+    templateUrl: './event-date.component.html',
+    styleUrls: ['./event-date.component.scss']
 })
 export class EventDateComponent implements OnInit {
+    _events: BehaviorSubject<any[]>;
 
-  constructor() { }
+    icon_search = faSearch;
 
-  ngOnInit() {
-  }
+    form: FormGroup;
 
+    constructor(
+        private route: ActivatedRoute,
+        private fb: FormBuilder,
+        private bs: BackendCaseService,
+        private loading: LoadingService,
+        @Inject(LOCALE_ID) private locale: string
+    ) {}
+
+    ngOnInit() {
+        this._events = new BehaviorSubject(new Array<any>());
+        /*this.route.data.subscribe((data: any) => {
+            this.events = data.events;
+        });*/
+
+        this.form = this.fb.group({
+            after_date: [],
+            after_time: [],
+            before_date: [],
+            before_time: []
+        });
+    }
+
+    get events(): any[] {
+        return this._events.value;
+    }
+
+    set events(data) {
+        this._events.next(data);
+    }
+
+    onSearch() {
+        if (!this.form.get('after_date').value) {
+            this.form.get('after_date').setValue(formatDate(new Date(), 'yyyy-MM-dd', this.locale));
+        }
+        const afterDate = this.form.get('after_date').value;
+
+        if (!this.form.get('after_time').value) {
+            this.form.get('after_time').setValue('00:00');
+        }
+        const afterTime = this.form.get('after_time').value;
+
+        if (!this.form.get('before_date').value) {
+            this.form.get('before_date').setValue(afterDate);
+        }
+        const beforeDate = this.form.get('before_date').value;
+
+        if (!this.form.get('before_time').value) {
+            this.form.get('before_time').setValue('23:59');
+        }
+        const beforeTime = this.form.get('before_time').value;
+
+        const after = formatDate(new Date(afterDate + ' ' + afterTime), 'yyyy-MM-dd\THH:mm:ss', this.locale);
+        const before = formatDate(new Date(beforeDate + ' ' + beforeTime), 'yyyy-MM-dd\THH:mm:ss', this.locale);
+        console.log(this.form.getRawValue());
+
+        this.loading.showLoading();
+        this.bs
+            .getEvents({ type: 'date', start: after, end: before })
+            .subscribe(result => {
+                this.events = result;
+            });
+    }
 }
